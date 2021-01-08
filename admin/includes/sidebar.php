9e15ca70cc4f0a30d1ea3b57f92b742cbd991472
aasif-ml <64016236+aasif-ml@users.noreply.github.com>
2021-01-08 21:04:44 +0530

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,58 @@
+﻿     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Admin</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                   <!-- <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons"></i>Dashboard</a></li>-->
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i>Election<i class="nav-drop-icon material-icons"></i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Add Election</a></li>
+                                <li><a href="managedepartments.php">Manage Election</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons"></i>Candidate<i class="nav-drop-icon material-icons"></i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Add Candidate</a></li>
+                                <li><a href="manageleavetype.php">Manage Candidate</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                   
+
+  
+<li class="no-padding"><a class="waves-effect waves-grey" href="attendances.php"><i class="material-icons"></i>Results</a></li>
+
+                         
+                 
+
+  <li class="no-padding"><a class="waves-effect waves-grey" href="changepassword.php"><i class="material-icons"></i>Change Password</a></li>
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons"></i>Sign Out</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                 <!--   <p class="copyright"><a href="http://phpgurukul.com/">e-voting </a>©</p>-->
+                
+                </div>
+                </div>
+            </aside>
